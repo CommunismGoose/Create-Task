a5e00f512a4d33fbd794b48dd877f5649c76fbde
Lacey <77512119+CommunismGoose@users.noreply.github.com>
2023-03-21 10:51:53 -0500

    added tic tac toe still need to add the win checks

--- src/main.py
+++ src/main.py
@@ -4,8 +4,41 @@ play=True
 #defining the functions
 #def clear():
     #os.system('cls')
-def tictactoe():
-    print(f' | | \n-----\n | | \n-----\n | | ')
+def tictactoe(): #plan is to make it so that the user input changes the move to an x or an o
+    totalmoves=0
+    board=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    previousmoves=[]
+    usermoves=[]
+    compmoves=[]
+    print('the following is a sample board with each postion numbered. PLease input a number to make a move\n1|2|3\n-----\n4|5|6\n-----\n7|8|9\n')
+    while totalmoves<5:
+        playermove=int(input(f'please input your move\nthe following is the board\n{board[0]}|{board[1]}|{board[2]}\n-----\n{board[3]}|{board[4]}|{board[5]}\n-----\n{board[6]}|{board[7]}|{board[8]}\n'))-1
+        if playermove not in previousmoves:
+            previousmoves.append(playermove)
+            usermoves.append(playermove)
+            board[playermove]='x'
+            totalmoves+=1
+            while True:
+                computermove=randint(0,8)
+                if totalmoves==5:
+                    break
+                if computermove not in previousmoves:
+                    previousmoves.append(computermove)
+                    compmoves.append(computermove)
+                    board[computermove]='o'
+                    break
+        else:
+            print('that is not a legal move go again')
+    #board variable that is changed based on numbers in a list
+    #list that stores the number
+    #while loop
+    #number that is inputed
+    #if statment that checks that the number is already played
+    #if not adds to previous move list
+    #append the board
+    #print the board
+    #check for 3 in a row
+    #continue is no 3 in a row
 def connect4():
     print('working on')
 def rps():#plan to make cleaner later
