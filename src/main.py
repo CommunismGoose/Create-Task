32aacb366e881bbf14285469361bdd6ee8f9c1d9
Lacey <77512119+CommunismGoose@users.noreply.github.com>
2023-03-20 22:55:59 -0500

    Did most of RPS
    i plan to make RPS Cleaner later on

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
+import os
+from random import randint
 play=True
 #defining the functions
+def clear():
+    os.system('cls')
 def tictactoe():
     print(f' | | \n-----\n | | \n-----\n | | ')
 def connect4():
     print('working on')
-def rps():
-    print('ez')
+def rps():#plan to make cleaner later
+	while True
+		v=0
+		movelist=['rock','sciscors','paper','rock']
+		compmove=movelist[randint(0,2)]
+		rpsmove=input('what is your move Rock or Paper or Sciscors").lower()
+		for i in movelist:
+			if v==1:
+				if i==rpsmove:
+					  print(f'you went {rpsmove}, computer went{compmove}.\nCongrats you win.')
+					  break
+				else:
+					  print(f'you went {rpsmove}, computer went{compmove}.\nI\'m sorry you lose.')
+					  break
+			if i==compmove:
+				v=1
+		if input('would you like to play again? y/n').lower() == 'n':
+			Break
 def chess():
     print('chess board coming soon')
 #core gameplay loop
