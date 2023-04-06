34250f64a4ba6548f0df718ea5c3b4308f3fea73
Lacey <77512119+CommunismGoose@users.noreply.github.com>
2023-04-06 10:19:24 -0500

    Commented RPS

--- src/RPS.py
+++ src/RPS.py
@@ -1,19 +1,19 @@
 from random import randint
-def rps():#plan to make cleaner later
+def rps():
 	while True:
-		v=0
-		movelist=['rock','sciscors','paper','rock']
-		compmove=movelist[randint(0,2)]
-		rpsmove=input('what is your move Rock or Paper or Sciscors\n').lower()
-		for i in movelist:
-			if v==1:
-				if i==compmove:
-					print(f'you went {rpsmove}, computer went {compmove}.\nCongrats you win.\n')
+		v=0 #random ahh variable
+		movelist=['rock','sciscors','paper','rock'] #possible movess
+		compmove=movelist[randint(0,2)] #the computers random move
+		rpsmove=input('what is your move Rock or Paper or Sciscors\n').lower() #the user move
+		for i in movelist: # for item in possible moves
+			if v==1: #if this is the item after usermove
+				if i==compmove: #if this move is computer move
+					print(f'you went {rpsmove}, computer went {compmove}.\nCongrats you win.\n') 
 					break
-				else:
+				else: #if this is a tie or lose
 					print(f'you went {rpsmove}, computer went {compmove}.\nI\'m sorry you didn\'t win\n.')
 					break
-			if i==rpsmove:
+			if i==rpsmove: #if item is usermove
 				v=1
-		if input('would you like to play again? y/n\n').lower() == 'n':
+		if input('would you like to play again? y/n\n').lower() == 'n': #play again?
 			break
